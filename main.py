f28389601dd78f8c03c3f2923bda7afda5a5bff1
shouc <shouchaofan@shphschool.com>
2018-08-28 18:46:03 -0700

    Add send part & use abstract syntax tree

--- main.py
+++ main.py
@@ -1,125 +1,220 @@
-import re, os, sys
+import re, os, sys, ast, random
+import astunparse
 from onelinerizer import onelinerize
 
 try: 
-	import terminaltables
+    import terminaltables
 except:
-	print "Oops"
-
-supportedOS = ["Windows", "Darwin"]
+    print "Oops"
+ignoreFiles = [".DS_Store", "__init__.py", "config.py"]
+ignoreEnds = ["pyc"]
+supportedOS = ["Windows", "Darwin", "*"]
 
 class utils():
-	@staticmethod
-	def checkFile(fileLines, sender = False):
-		_regex = "__%s__.*?=.*?\"(.*?)\""
-		_sys = re.compile(_regex % "sys")
-		_name = re.compile(_regex % "name")
-		_desc = re.compile(_regex % "desc")
-		resultTemp = {
-			"success": 0,
-			"info": "None",
-			"sys": "",
-			"name": "",
-			"desc": "",
-			"code": ""
-		}
-		for i in enumerate(fileLines):
-			if ";" not in i[1]:
-				if not resultTemp["sys"]:
-					lineSys = _sys.findall(i[1])
-					if lineSys:
-						resultTemp["sys"] = lineSys[0]
-						continue
-				if not resultTemp["name"]:
-					lineName = _name.findall(i[1])
-					if lineName:
-						resultTemp["name"] = lineName[0]
-						continue
-				if not resultTemp["desc"]:
-					lineDesc = _desc.findall(i[1])
-					if lineDesc:
-						resultTemp["desc"] = lineDesc[0]
-						continue
-			else:
-				trivialResult = utils.checkFile(i[1].split(";"))
-				for j in trivialResult:
-					if not resultTemp[j] and trivialResult[j]:
-						resultTemp[j] = trivialResult[j]
-		return resultTemp
+    @staticmethod
+    def checkFile(fileLines, sender = False):
+        _regex = "__%s__.*?=.*?\"(.*?)\""
+        _sys = re.compile(_regex % "sys")
+        _name = re.compile(_regex % "name")
+        _desc = re.compile(_regex % "desc")
+        resultTemp = {
+            "success": 0,
+            "info": "None",
+            "sys": "",
+            "name": "",
+            "desc": "",
+            "sendCode": "",
+            "sendCodeFunc": ""
+        }
+        for i in enumerate(fileLines):
+            if ";" not in i[1]:
+                if not resultTemp["sys"]:
+                    lineSys = _sys.findall(i[1])
+                    if lineSys:
+                        resultTemp["sys"] = lineSys[0]
+                        continue
+                if not resultTemp["name"]:
+                    lineName = _name.findall(i[1])
+                    if lineName:
+                        resultTemp["name"] = lineName[0]
+                        continue
+                if not resultTemp["desc"]:
+                    lineDesc = _desc.findall(i[1])
+                    if lineDesc:
+                        resultTemp["desc"] = lineDesc[0]
+                        continue
+            else:
+                trivialResult = utils.checkFile(i[1].split(";"))
+                for j in trivialResult:
+                    if not resultTemp[j] and trivialResult[j]:
+                        resultTemp[j] = trivialResult[j]
+        return resultTemp
 
-	@staticmethod
-	def checkContent(info, file):
-		if info["sys"] not in supportedOS:
-			info["success"] = 1
-			info["info"] = "OS not Supported"
-			print "[!] OS not Supported for file %s" % file
-		for k in info:
-			if not info[k] and k in ["sys", "name", "desc"]:
-				info["success"] = 2
-				info["info"] = k
-				print "[!] No %s has been given in file %s" % (info["info"], 
-					file)
-		if " " in info["name"]:
-			info["success"] = 3
-			info["info"] = "Space In Name"
-			print "[!] There should be no space in name of %s" % file
-		return info
+    @staticmethod
+    def checkContent(info, file):
+        if info["sys"] not in supportedOS:
+            info["success"] = 1
+            info["info"] = "OS not Supported"
+            print "[!] OS not Supported for file %s" % file
+        for k in info:
+            if not info[k] and k in ["sys", "name", "desc"]:
+                info["success"] = 2
+                info["info"] = k
+                print "[!] No %s has been given in file %s" % (info["info"], 
+                    file)
+        if " " in info["name"]:
+            info["success"] = 3
+            info["info"] = "Space In Name"
+            print "[!] There should be no space in name of %s" % file
+        return info
 
-	@staticmethod
-	def checkInt(info, file):
-		try:
-			code = onelinerize(open(file).read())
-			if " send.__name__" in code:
-				exec(code)
-				info["code"] = code
-			else:
-				info["success"] = 5
-				info["info"] = "No Send Func"
-				print "[!] No 'send' function in file %s" % file
-		except:
-			info["success"] = 4
-			info["info"] = "Error"
-			print "[x] Error %s in file %s" % (sys.exc_info()[0], file)
-		return info
-	@staticmethod
-	def checkModule(info, file):
-		return utils.checkContent(
-			utils.checkInt(info, file), 
-			file)
+    @staticmethod
+    def checkModuleInt(info, file):
+        try:
+            code = open(file).read()
+            for i in ast.walk(ast.parse(code)):
+                try:
+                    if isinstance(i, ast.FunctionDef):
+                        funcCode = astunparse.unparse(i)
+                        reSendFunc = re.compile("def.+?send\(")
+                        if reSendFunc.findall(funcCode):
+                            funcName = 'send%s%s' % (
+                                info["name"], str(random.randint(0,2000)))
+                            sendCode = onelinerize(reSendFunc.sub('def %s(' % funcName,
+                                funcCode))
+                            exec(sendCode)
+                            info["sendCode"] = sendCode
+                            info["sendCodeFunc"] = funcName
+
+                except:
+                    info["success"] = 4
+                    info["info"] = "Error"
+                    print "[x] Error %s in file %s" % (sys.exc_info()[0], file)
+
+            if info["sendCode"] == "":
+                info["success"] = 5
+                info["info"] = "No Send Func"
+                print "[!] No 'send' function in file %s" % file
+        except:
+            info["success"] = 4
+            info["info"] = "Error"
+            print "[x] Error %s in file %s" % (sys.exc_info()[0], file)
+        return info
+    @staticmethod
+    def checkModule(info, file):
+        return utils.checkContent(
+            utils.checkModuleInt(info, file), 
+            file)
+    @staticmethod
+    def checkSendInt(info, file):
+        try:
+            code = open(file).read()
+            for i in ast.walk(ast.parse(code)):
+                try:
+                    if isinstance(i, ast.FunctionDef):
+                        funcCode = astunparse.unparse(i)
+                        reSendFunc = re.compile("def.+?send\(")
+                        reReceiveFunc = re.compile("def.+?receive\(")
+                        if reSendFunc.findall(funcCode):
+                            funcName = 'send%s%s' % (
+                                info["name"], str(random.randint(0,2000)))
+                            sendCode = onelinerize(reSendFunc.sub('def %s(' % funcName,
+                                funcCode))
+                            exec(sendCode)
+                            info["sendCode"] = sendCode
+                            info["sendCodeFunc"] = funcName
+                        elif reReceiveFunc.findall(funcCode):
+                            funcName = 'receive%s%s' % (
+                                info["name"], str(random.randint(0,2000)))
+                            receiveCode = onelinerize(reSendFunc.sub('def %s(' % funcName,
+                                funcCode))
+                            exec(receiveCode)
+                            info["receiveCode"] = receiveCode
+                            info["receiveCodeFunc"] = funcName
+
+                except:
+                    info["success"] = 4
+                    info["info"] = "Error"
+                    print "[x] Error %s in file %s" % (sys.exc_info()[0], file)
+
+            if info["sendCode"] == "":
+                info["success"] = 5
+                info["info"] = "No Send Func"
+                print "[!] No 'send' function in file %s" % file
+
+            if info["receiveCode"] == "":
+                info["success"] = 5
+                info["info"] = "No Receive Func"
+                print "[!] No 'receive' function in file %s" % file
+        except:
+            info["success"] = 4
+            info["info"] = "Error"
+            print "[x] Error %s in file %s" % (sys.exc_info()[0], file)
+        return info
+
+    @staticmethod
+    def checkSend(info, file):
+        return utils.checkContent(
+            utils.checkSendInt(info, file), 
+            file)
+
 
 class API():
-	@staticmethod
-	def getModuleInfo(path='module'):
-		moduleInfo = []
-		for dirpath,dirnames,filenames in os.walk(path):
-			for file in filenames:
-				fullpath = os.path.join(dirpath,file)
-				moduleInfo.append(utils.checkModule(
-					utils.checkFile(
-						open(fullpath).readlines()
-					),fullpath
-				))
-		return moduleInfo
+    @staticmethod
+    def getModuleInfo(path='module'):
+        moduleInfo = []
+        for dirpath,dirnames,filenames in os.walk(path):
+            for file in filenames:
+                fullpath = os.path.join(dirpath,file)
+                if not (file in ignoreFiles \
+                    or file.split(".")[-1] in ignoreEnds):
+                    moduleInfo.append(utils.checkModule(
+                        utils.checkFile(
+                            open(fullpath).readlines()
+                        ),fullpath
+                    ))
+        return moduleInfo
+
+    @staticmethod
+    def getSendInfo(path='messenger'):
+        sendInfo = []
+        for dirpath,dirnames,filenames in os.walk(path):
+            for file in filenames:
+                fullpath = os.path.join(dirpath,file)
+                if not (file in ignoreFiles \
+                    or file.split(".")[-1] in ignoreEnds):
+                        sendInfo.append(utils.checkSend(
+                            utils.checkFile(
+                                open(fullpath).readlines()
+                            ),fullpath
+                        ))
+        return sendInfo
+
+print API.getSendInfo()
 
 class beautify():
-	@staticmethod
-	def getModuleInfo(path='module'):
-		info = API.getModuleInfo(path)
-		result = [["Name", "Description", "SupportedOS", "Status"]]
-		for i in info:
-			if i["success"] == 0:
-				result.append([i["name"], i["desc"], i["sys"], "OK"])
-			else:
-				result.append(
-					[i["name"], i["desc"], i["sys"], "Failed"]
-				)
-		try:
-			return terminaltables.AsciiTable(result).table
-		except:
-			print "Error! Did you install terminaltables"
+    @staticmethod
+    def b(info):
+        result = [["Name", "Description", "SupportedOS", "Status"]]
+        for i in info:
+            if i["success"] == 0:
+                result.append([i["name"], i["desc"], i["sys"], "OK"])
+            else:
+                result.append(
+                    [i["name"], i["desc"], i["sys"], "Failed"]
+                )
+        try:
+            return terminaltables.AsciiTable(result).table
+        except:
+            print "Error! Did you install terminaltables"
+    
+    @staticmethod
+    def getModuleInfo(path='module'):
+        info = API.getModuleInfo(path)
+        return beautify.b(info)
 
-
-
-print beautify.getModuleInfo()
-
-
+    @staticmethod
+    def getSendInfo(path='messenger'):
+        info = API.getModuleInfo(path)
+        return beautify.b(info)
+        
