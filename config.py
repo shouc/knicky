24ce97bca43efa4b5b168ab5c0af39cb9c2df9e1
shouc <shouchaofan@shphschool.com>
2018-09-22 14:59:04 +0800

    pylint update

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 import core
 import sha1Info
 
-def sgUpdate(bypass = False, showInfo = False,
-    _user = "None", _password = "None", 
-    _server = "None", _apiKey = "None"
-    ):
+def sgUpdate(showInfo=False, _user="None",
+    _password="None", _server="None", 
+    _apiKey="None"):
     updateList = [
         {"original":"user", "after":_user, "desc":"The Email Address"},
         {"original":"password", "after":_password, "desc":"The Password of that Email Address"},
@@ -15,12 +14,11 @@ def sgUpdate(bypass = False, showInfo = False,
         return core.beautify.showInfo(updateList)
     try:
         temp = open("messenger/sg.uninit").read()
-        if not bypass:
-            if core.utils.getSHA1(temp) == sha1Info.sg:
-                core.utils.updateFile("messenger/sg.uninit", 
-                    updateList)
-            else:
-                print core.utils.getSHA1(temp)
-                return "[!] Error! Template file is not correct"
+        if core.utils.getSHA1(temp) == sha1Info.sg:
+            core.utils.updateFile("messenger/sg.uninit",
+                updateList)
+        else:
+            return ("[!] Error! Template file (%s) is not correct"
+                % core.utils.getSHA1(temp))
     except:
         return "[!] Error! No template file found"
