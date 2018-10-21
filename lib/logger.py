7074801dbcd19d4377d3db8d66dace5b818dc6de
Shou C <shouchaofan@shphschool.com>
2018-10-21 15:36:52 +0800

    typo fix

--- lib/logger.py
+++ lib/logger.py
@@ -1,20 +1,18 @@
 # -*- coding:utf-8 -*-
 
 class log(object):
-
-    WARRING_COLOR = '\033[1;37m'  # yellow
-    ERROR_COLOR   = '\033[1;31m'  # red
-    INFO_COLOR    = '\033[1;34m'  # blue
-    PASS_COLOR    = '\033[1;32m'  # green
+    WARNING_COLOR = '\033[1;37m' 
+    ERROR_COLOR   = '\033[1;31m'
+    INFO_COLOR    = '\033[1;34m'
+    PASS_COLOR    = '\033[1;32m'
     END_COLOR = '\033[0m'
-
     @staticmethod
     def logError(msg):
         print("[Error]: %s%s%s"%(log.ERROR_COLOR, msg, log.END_COLOR))
-    
+
     @staticmethod
     def logWarn(msg):
-        print("[Warning]: %s%s%s"%(log.WARRING_COLOR, msg, log.END_COLOR))
+        print("[Warning]: %s%s%s"%(log.WARNING_COLOR, msg, log.END_COLOR))
 
     @staticmethod
     def logInfo(msg):
